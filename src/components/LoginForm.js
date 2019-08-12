56c4fe3be2fff12a2bd06df516bb5190a30f4bb6
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-08-12 09:12:56 +0400

    Create logina and signup form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,17 @@
 import React from 'react';
-//import LoginForm from './src/components/LoginForm';
-//import App from './components/App';
+import LoginForm from './src/components/LoginForm';
+import App from './components/App';
 
 class LoginForm extends React.Component{
         render(){
             return(
-            <>
+            <React.Fragment className="login-form">
             <input type="text" placeholder="Username"/>
             <input type="text" placeholder="Password"/>
             <a href="#">Login</a>
             <button>Submit</button>
-            </>
+            </React.Fragment>
         );
 } 
 }
-//default export LoginForm;
+default export LoginForm;
