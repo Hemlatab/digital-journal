c753b254919a45a7f429acdd843608c1ca894994
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-08-17 20:49:48 +0400

    Add styling to pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-import LoginForm from './LoginForm';
-import SignupForm from './SignupForm';
+// import LoginForm from './LoginForm';
+// import SignupForm from './SignupForm';
 import AddJournalForm from './AddJournalForm';
 //import Footer from './Footer';
 
@@ -13,8 +13,7 @@ function App() {
      <Header />
      <AddJournalForm />
      <br />
-     <SignupForm />
-     <LoginForm />
+
       {/* <Footer /> */}
     </div>
   );
