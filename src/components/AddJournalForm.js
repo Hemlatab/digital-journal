27dbf1ffca129577012bd5574c8a703b33057d73
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-08-18 16:20:58 +0400

    Refactor add journal form page

--- src/components/AddJournalForm.js
+++ src/components/AddJournalForm.js
@@ -1,32 +1,18 @@
 import React from "react";
 import './AddJournalForm.css';
 
-class AddJournalForm extends React.Component{
+const AddJournalForm =(props)=>{
 
-    state ={
-        title:'',
-        body:''
-    }
-
-    handleChange = (event) => {
-
-        this.setState({[event.target.name]:event.target.value})
-    }
-    handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
-    }
-    render(){
-
-        const {title,body} = this.state
+    
+        const {handleChange,handleSubmit,title,body} = props
         return(
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" className="journal-title" name="title" value={title} onChange={this.handleChange} placeholder="Title"/>
-                <textarea rows="5" placeholder="Body" name="body" value={body} onChange={this.handleChange}></textarea>
+            <form onSubmit={handleSubmit}>
+                <input type="text" className="journal-title" name="title" value={title} onChange={handleChange} placeholder="Title"/>
+                <textarea rows="5" placeholder="Body" name="body" value={body} onChange={handleChange}></textarea>
                 <p className="body-para">Use the form above to create a post. Make sure you fill the required title and body field and then press submit.</p>
                 <button type="submit" className="submit-post-btn">Submit</button>
             </form>
         )
-    }
+    
 }
 export default AddJournalForm;
