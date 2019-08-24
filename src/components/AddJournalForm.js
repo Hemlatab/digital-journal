e23682ab049457f1c1f4e31e002ea04a1840b999
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-08-24 23:42:48 +0400

    Add styles

--- src/components/AddJournalForm.js
+++ src/components/AddJournalForm.js
@@ -11,7 +11,8 @@ const AddJournalForm =(props)=>{
                 <h2 className="note-title">Create a Note</h2>
                 <input type="text" className="journal-title" name="title" value={title} onChange={handleChange} placeholder="Title"/>
                 <textarea rows="5" placeholder="Body" name="body" value={body} onChange={handleChange}></textarea>
-                <p className="body-para">Use the form above to create a post. Make sure you fill the required title and body field and then press submit.</p>
+                <p className="body-para">
+                Use the form above to create a post. Make sure you fill the required title and body field and then press submit.</p>
                 <Button label="Submit"/>
             </form>
         )
